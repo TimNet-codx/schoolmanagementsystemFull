2be09006ff7a3bbcff47dd9d7755635656bf5483
TimNet-codx <jesulayomitimze55@gmail.com>
2026-08-25 05:03:11 +0100

    update V1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,23 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+const isProtectedRoute = createRouteMatcher([
+  "/admin",
+  "/teacher",
+  "/student",
+  "/parent",
+]);
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth.protect();
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
     // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
+    // "/__clerk/(.*)",
   ],
-}
-
+};
